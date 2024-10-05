697ebc990ffa9cb38784b8ef721699fdb865a072
YeshMeAin <yonatan.hadas.1008@gmail.com>
2024-10-05 23:47:26 +0300

    [API] basic JWT auth setup

--- api/sultana-api/config/routes.rb
+++ api/sultana-api/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :customers
   resources :menu_items
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  # root "posts#index"  # Other routes
 end
