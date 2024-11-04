c70ab45280cd8ad5900aa8c3747397d6d8500380
YeshMeAin <yonatan.hadas.1008@gmail.com>
2024-11-04 09:16:03 +0200

    actually working form

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,7 @@
 class Menu < ApplicationRecord
   has_many :menu_items, dependent: :destroy
+  accepts_nested_attributes_for :menu_items, reject_if: :all_blank, allow_destroy: true
+
   has_many :items, through: :menu_items
 
   validates :name, presence: true
