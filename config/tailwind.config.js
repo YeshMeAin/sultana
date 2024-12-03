bb16fb96ccf5b81c58cc9255e60f12694f9be3c3
YeshMeAin <yonatan.hadas.1008@gmail.com>
2024-12-04 01:20:13 +0200

    fully formed orders UI

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -18,5 +18,12 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
+    function({ addComponents }) {
+      addComponents({
+        'input[type="datetime-local"]': {
+          '@apply mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm': {}
+        }
+      })
+    }
   ]
 }
