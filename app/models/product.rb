6cdd735c4693b962a48176741fc6f4d9d9238a7c
YeshMeAin <yonatan.hadas.1008@gmail.com>
2024-12-31 23:10:33 +0200

    products as resource

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 class Product < ApplicationRecord
+  include ResourceAttributes
+
   has_many :receipes, dependent: :destroy
   has_many :items, through: :receipes
 
   validates :name, presence: true
   validates :name, uniqueness: true
+
+  def self.table_attributes
+    [:name, :price_per_unit, :units, :in_bulk]
+  end
+
+  def self.show_attributes
+    [:name, :price_per_unit, :units, :in_bulk]
+  end
 end
