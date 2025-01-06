6fe2038cda860235a56ef5b027c191e8a35f65b5
YeshMeAin <yonatan.hadas.1008@gmail.com>
2025-01-06 09:26:43 +0200

    Add ordering to associated collections

--- app/models/order.rb
+++ app/models/order.rb
@@ -83,6 +83,7 @@ class Order < ApplicationRecord
       name: 'Order Items',
       collection: order_items.joins(:item)
         .select('items.name AS item_name, order_items.price AS item_price, order_items.quantity, (order_items.quantity * order_items.price) AS total_price, order_items.notes, order_items.oth')
+        .order('items.name')
     }]
   end
 
