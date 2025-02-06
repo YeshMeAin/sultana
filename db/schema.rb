c6fea35576d32d9f7a688c09a370a5ee24ed62f5
YeshMeAin <yonatan.hadas.1008@gmail.com>
2025-02-06 21:26:12 +0200

    shcema changes after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_02_072324) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_192406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -96,7 +96,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_02_072324) do
     t.string "name"
     t.string "units"
     t.float "price_per_unit"
-    t.boolean "in_bulk"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_products_on_name"
