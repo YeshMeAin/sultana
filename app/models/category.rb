219964e6f65677a6e407f4663484811efd69f0d5
YeshMeAin <162214383+YeshMeAin@users.noreply.github.com>
2025-04-03 21:53:14 +0300

    Passover menu adjustments (#19)

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,10 +6,10 @@ class Category < ApplicationRecord
   validates :display_name, :slug, presence: true
 
   def self.table_attributes
-    [:display_name, :slug, :is_active, :sort_order]
+    [:display_name, :slug, :is_active, :sort_order, :description]
   end
 
   def self.show_attributes
-    [:display_name, :slug, :is_active, :sort_order, :updated_at, :created_at]
+    [:display_name, :slug, :is_active, :sort_order, :description, :updated_at, :created_at]
   end
 end
